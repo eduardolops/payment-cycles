d7e16fe8a20a23941eefcbfa5e6ea6f3778a0d60
Eduardo Lopes <edulopestj@gmail.com>
2017-07-25 22:29:45 -0300

    making some adjustments

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 module.exports = (app) => {
     
     const Router = express.Router()
-    app.user('/api', Router)
+    app.use('/api', Router)
 
     const BillingCycle = require('../api/billingCycle/billingCycleServices')
     BillingCycle.register(Router, '/billing-cycles')
