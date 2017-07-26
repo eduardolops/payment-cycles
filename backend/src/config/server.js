fb9fc4a51c0120fc37c66825ad7738e349da04db
Eduardo Lopes <edulopestj@gmail.com>
2017-07-25 23:41:36 -0300

    add cors and query parser

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,14 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
+const queryParser = require('express-query-int')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
+app.use(allowCors)
+app.use(queryParser())
 
 app.listen(port, function(){
     console.log(`BACKEND is runing on port ${port}`)
