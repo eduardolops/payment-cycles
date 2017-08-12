ac56820e558e9a54ba57ce541ffbc9bb59601bf9
Eduardo Lopes <edulopestj@gmail.com>
2017-08-11 23:28:51 -0300

    finished configuration footer of template

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strog>
+            Copyright &copy; 2017
+            <a href="https://github.com/eduardolops" target="_blank"> Eduardo Lopes</a>
+        </strog>
+    </footer>
+)
