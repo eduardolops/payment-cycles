eeed77c513ca3eff66fcfe8cbf796e88076a8102
Eduardo Lopes <edulopestj@gmail.com>
2017-08-31 21:43:06 -0300

    add tab content

--- frontend/src/common/tab/tabReducer.js
+++ frontend/src/common/tab/tabReducer.js
@@ -1,7 +1,7 @@
 const INITIAL_STATE = { selected: '' }
 
 export default ( state = INITIAL_STATE, action ) => {
-    switch(action){
+    switch(action.type){
         case 'TAB_SELECTED': 
             return { ...state, selected: action.payload }
         default: 
